9a68f3ea0113fcd1e2b038d97bd93f5fbab48d73
solovyovsdom <joelborofsky@mac.com>
2022-10-14 21:38:24 -0500

    got the basic styling down for colors

--- script.js
+++ script.js
@@ -1,37 +1,35 @@
 const colorScheme = document.getElementById('color-picker');
 
+let colorsArray = [];
+let colorImg = '';
 
-let colorsArray = []
-let colorImg = ''
-
-function renderColors(){
-    let html = ""
-    for (let color of colorsArray) {
-        html += `
-            <img src='${colorImg}' />
-           <p>${color.hex.value}</p>
-        `
-        
-    }
-    document.getElementById('color-container').innerHTML = html
-
+function renderColors() {
+  let colors = '';
+  let codes = ''
+  for (let color of colorsArray) {
+    colors += `<div class="colors" style="background-color:${color.hex.value}"></div>`;
+    codes += `<p  class="color-codes">${color.hex.value}</p>`;
+  }
+  document.getElementById('colors-container').innerHTML = colors;
+  document.getElementById('code-container').innerHTML = codes;
 }
 
 colorScheme.addEventListener('submit', (e) => {
   e.preventDefault();
   let color = document.getElementById('color').value.replace('#', '');
-  let scheme = document.getElementById('schemes').value
-  let quantity = document.getElementById('quantity').value
-  color.replace('#',"")
-  fetch(`https://www.thecolorapi.com/scheme?hex=${color}&mode=${scheme}&count=${quantity}`)
+  let scheme = document.getElementById('schemes').value;
+  let quantity = document.getElementById('quantity').value;
+  color.replace('#', '');
+  fetch(
+    `https://www.thecolorapi.com/scheme?hex=${color}&mode=${scheme}&count=${quantity}`
+  )
     .then((res) => res.json())
     .then((data) => {
-        colorsArray = data.colors
-        colorImg = data.image.named
-        renderColors()
-       console.log(data)
-      
-    }); 
-    
+      colorsArray = data.colors;
+      colorImg = data.image.named;
+
+      renderColors();
+    });
 });
 
+// style = 'background-color:${color.hex.value}';
